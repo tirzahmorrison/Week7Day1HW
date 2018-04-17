b2475697ec636da7035638c6761d5c87e6fda584
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-04-16 21:21:55 -0400

    hi

---
+++ Week7Day1HW/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Week7Day1HW
+{
+    public class Movie
+    {
+        public Movie() { }
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int YearReleased { get; set; }
+        public string Genre { get; set; }
+        public string Tagline { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
--- Week7Day1HW/Program.cs
+++ Week7Day1HW/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+
 
 namespace Week7Day1HW
 {
@@ -10,6 +12,31 @@ namespace Week7Day1HW
     {
         static void Main(string[] args)
         {
+            using (var db = new DataBaseContext())
+            {
+                var movie = new Movie
+                {
+                    Title = "The Never Ending Story",
+                    YearReleased = 1988,
+                    Genre = "Horror",
+                    Tagline = "Said your mom",
+                    Rating = 9.8m,
+                };
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                var movies = db.Movies.Where(m => m.YearReleased == 1988);
+                foreach (var m in movies)
+                {
+                    m.Rating = 10m;
+                }
+                db.SaveChanges();
+                var horror = db.Movies.Where(h => h.Genre == "Horror");
+                var bestMovieInTheWorld = db.Movies.First(n => n.Title == "The Never Ending Story");
+                db.Movies.Remove(bestMovieInTheWorld);
+                db.SaveChanges();
+            }
+            Console.WriteLine("Hi, I'm watching you");
+            Console.ReadLine();
         }
     }
 }
